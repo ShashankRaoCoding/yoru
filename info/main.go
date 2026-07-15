03f7e1239e91563d67d381e3cb886f2ac52a9e61
ShashankRao <158217706+ShashankRaoCoding@users.noreply.github.com>
2026-07-15 13:30:27 +0100

    Add: Create missing info package

---
+++ info/main.go
@@ -0,0 +1,13 @@
+package info
+
+import (
+	"fmt"
+)
+
+func Main(args []string) {
+	fmt.Println("yoru - A personal library of binaries")
+	fmt.Println("\nAvailable commands:")
+	fmt.Println("  make  - Create new files with empty lines")
+	fmt.Println("  sql   - Convert CSV to SQL and query")
+	fmt.Println("  info  - Display this help message")
+}
