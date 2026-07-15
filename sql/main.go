6e7c540d12429f926e1b33ca50a91a81e9e35d07
ShashankRao <158217706+ShashankRaoCoding@users.noreply.github.com>
2026-07-15 14:31:57 +0100

    Fix: Read SQL query from os.Args instead of file descriptor 3

--- sql/main.go
+++ sql/main.go
@@ -1,19 +1,23 @@
 package sqlpkg
 
 import (
-	"io"
+	"fmt"
 	"os"
 	"yoru/globals"
 	"yoru/utils"
 )
 
 func Main(args []string) {
+	if len(args) < 1 {
+		utils.Error(fmt.Errorf("usage: sql <query>"))
+		return
+	}
+
+	query := args[0]
+
 	db, err := ImportCSVToSQL(os.Stdin, globals.TEMP, "table")
 	utils.Error(err)
 	
-	query, err := io.ReadAll(os.NewFile(3, "QUERY"))
-	utils.Error(err)
-	
-	err = QueryToCSV(db, string(query), os.Stdout)
+	err = QueryToCSV(db, query, os.Stdout)
 	utils.Error(err)
 }
